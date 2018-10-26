2210e4340b5da4baf425e8318f80e390188c13d1
sunkstingray <lynam.chris@gmail.com>
2018-10-26 18:03:52 -0500

    added background image and some carousel content

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -9,5 +9,22 @@ $(document).ready(function () {
                 $(".navbar-burger").toggleClass("is-active");
                 $(".navbar-menu").toggleClass("is-active");
         });
+
+        $(".navbar-item").click(function () {
+                $(".navbar-burger").toggleClass("is-active");
+                $(".navbar-menu").toggleClass("is-active");
+        });
+
+        $("#about-me").click(function () {
+                $(".modal").toggleClass("is-active");
+        });
+
+        $(".delete").click(function () {
+                $(".modal").toggleClass("is-active");
+        });
+
+        $(".modal-background").click(function () {
+                $(".modal").toggleClass("is-active");
+        });
 });
 
