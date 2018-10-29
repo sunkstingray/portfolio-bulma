9714664ee7c69e8f1936a7b8888c66cd09b92821
sunkstingray <lynam.chris@gmail.com>
2018-10-29 16:51:50 -0500

    slide order, responsive adjustments

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -30,9 +30,11 @@ $(document).ready(function () {
                                 breakpoint: 1024,
                                 settings: {
                                         slidesToShow: 1,
-                                        slidesToScroll: 1
+                                        slidesToScroll: 1,
+                                        arrows: true
                                 }
                         }
+
                         // You can unslick at a given breakpoint now by adding:
                         // settings: "unslick"
                         // instead of a settings object
