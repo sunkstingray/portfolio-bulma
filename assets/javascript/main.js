c1ee2c25a5bd6275c38f150b93eaf1417444cfda
sunkstingray <lynam.chris@gmail.com>
2018-10-29 14:48:01 -0500

    carousel switched to slick, responsiveness added to caarousel, css updates

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,52 @@
 $(document).ready(function () {
-        var carousels = bulmaCarousel.attach(); // carousels now contains an array of all Carousel instances
+        // var carousels = bulmaCarousel.attach(); // carousels now contains an array of all Carousel instances
 
-        $(window).resize(function () {
-                carousels = bulmaCarousel.attach();
+        // $(window).resize(function () {
+        //         carousels = bulmaCarousel.attach();
+        // });
+
+        // $('#project-carousel').slick({
+        //         // setting-name: setting-value
+        // });
+
+        $('#project-carousel').slick({
+                dots: false,
+                arrows: false,
+                infinite: true,
+                speed: 300,
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                autoplay: true,
+                autoplaySpeed: 7000,
+                responsive: [
+                        {
+                                breakpoint: 1910,
+                                settings: {
+                                        slidesToShow: 3,
+                                        slidesToScroll: 3
+                                }
+                        },
+                        {
+                                breakpoint: 1024,
+                                settings: {
+                                        slidesToShow: 1,
+                                        slidesToScroll: 1
+                                }
+                        }
+                        // You can unslick at a given breakpoint now by adding:
+                        // settings: "unslick"
+                        // instead of a settings object
+                ]
         });
+
+        $('#left-button').on('click', function () {
+                $('#project-carousel').slick('slickPrev');
+        });
+
+        $('#right-button').on('click', function () {
+                $('#project-carousel').slick('slickNext');
+        });
+
 
         $(".burger").click(function () {
                 $(".navbar-burger").toggleClass("is-active");
